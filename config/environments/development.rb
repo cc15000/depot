995853fc89e1e5f8d91b3653589b2f21943f62c6
Chris Cocchiaraley <cc15000@gmail.com>
2012-01-03 00:02:25 -0500

    Sending Mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,16 @@
 Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address        => "smtp.gmail.com",
+    :port           => 587,
+    :domain         => "gmail.com",
+    :authentication => "plain",
+    :user_name      => "cc15000",
+    :password       => "lck1818shr",
+    :enable_starttls_auto => true
+  }
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
