e48795df71192ef606a193e0ad5af49df2ed91ba
Chris Cocchiaraley <cc15000@gmail.com>
2012-01-07 00:27:28 -0500

    internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_i18n_locale_from_params
+  before_filter :authorize
   protect_from_forgery
 
   private
@@ -10,4 +12,29 @@ class ApplicationController < ActionController::Base
       session[:cart_id] = cart.id
       cart
     end
+
+  protected
+
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.include?(params[:locale].to_sym)
+          I18n.locale = params[:locale]
+      else
+        flash.now[:notice] =
+          "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
+      end
+    end
+   
+    end
+
+    def default_url_options
+      { :locale => I18n.locale }
+    end
+
+    def authorize
+      unless User.find_by_id(session[:user_id])
+      redirect_to login_url, :notice => "Please log in"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,31 @@
 Depot::Application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  scope '(:locale)' do
+    resources :users
+    resources :orders
+    resources :line_items
+    resources :carts
+    resources :products do
+      get :who_bought, :on => :member
+    end
+    root :to => 'store#index', :as => 'store'
+  end
+
+  get "sessions/new"
+
+  get "sessions/create"
+
+  get "sessions/destroy"
+
+  resources :users
+
   resources :orders
 
   resources :line_items
